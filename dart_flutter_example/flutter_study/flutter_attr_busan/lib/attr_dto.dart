a3c688d8691712dce61e54291f284cd3e06dba0c
Chaeso99 <storymmm@naver.com>
2023-04-03 17:24:46 +0900

    23.04.03

--- dart_flutter_example/flutter_study/flutter_attr_busan/lib/attr_dto.dart
+++ dart_flutter_example/flutter_study/flutter_attr_busan/lib/attr_dto.dart
@@ -1,24 +1,34 @@
 class AttrDTO {
-  //MAIN_TITLE
-  //CNTCT_TEL
-  //MAIN_IMG_THUMB
+  // MAIN_TITLE
+  // CNTCT_TEL
+  // MAIN_IMG_THUMB
 
-  //카멜케이스 형식으로
+  //카멜케이스 형식
   String mainTitle;
   String cntctTel;
   String mainImgThumb;
 
-  AttrDTOs({
-  required this.mainTitle, required this.cntctTel, required this mainImgThumb,
-});
+  AttrDTO({
+    required this.mainTitle,
+    required this.cntctTel,
+    required this.mainImgThumb,
+  });
 
-  factory AttrDTO fromjson (dynamic json) => AttrDTO(
-  mainTitle : json["MAIN_TITLE"],
-  cntctTel : json["CNTCT_TEL"],
-  mainImgThumb: json["MAIN_IMG_THUMB"],
-)
-  static List<AttrDTO> fromJsonList(List json){
-    jsonList.map((e)=>AttrDTO.fromJson(e)).toList();
+  factory AttrDTO.fromJson(dynamic json) => AttrDTO(
+    mainTitle: json["MAIN_TITLE"],
+    cntctTel: json["CNTCT_TEL"],
+    mainImgThumb: json["MAIN_IMG_THUMB"],
+  );
 
+  static List<AttrDTO> fromJsonList(List jsonList){
+    return jsonList.map((e) => AttrDTO.fromJson(e)).toList();
+  }
 }
-}
+
+
+
+
+
+
+
+
--- dart_flutter_example/flutter_study/flutter_attr_busan/lib/attr_repository.dart
+++ dart_flutter_example/flutter_study/flutter_attr_busan/lib/attr_repository.dart
@@ -3,14 +3,14 @@ import 'dart:convert';
 import 'package:flutter_attr_busan/attr_dto.dart';
 import 'package:http/http.dart' as http;
 
-class AttrRepository{
-  static Future<AttrDTO?> getDTO() async{
-    final String url = "https://www.visitbusan.net/uploadImgs/files/cntnts/20191222164810529_thumbL";
+class AttrRepository {
+  static Future<List<AttrDTO>?> getDTO() async {
+    final String url = "https://apis.data.go.kr/6260000/AttractionService/getAttractionKr?serviceKey=g3%2FAuxiiARVuMl8t%2B2l9KVfFq2jcIu5MY6PkLcFKLJ3RZvz2UQDeTl1CCsbaxfrTw7BbYnPQlxkunGyPvdrx8A%3D%3D&pageNo=1&numOfRows=10&resultType=json";
     http.Response response = await http.get(Uri.parse(url));
-    if(response.statusCode==200){
-      jsonDecode(response.body);
-      return AttrDTO.fromJsonList(json);
-    }else {
+    if(response.statusCode == 200){
+      dynamic json = jsonDecode(response.body);
+      return AttrDTO.fromJsonList(json["getAttractionKr"]["item"]);
+    } else {
       return null;
     }
   }
